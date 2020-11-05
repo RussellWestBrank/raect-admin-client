3cada853da44a316845a8ffa012cf8d0ed6d559a
Russel <>
2020-11-05 22:20:13 +0800

    feed

---
+++ src/pages/login/index.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from "react"
+import "./login.less"
+import logo from "./images/logo.jpeg"
+import { Form, Icon, Input, Button } from "antd"
+/**
+ * 登陆路由组件
+ */
+
+export default class Login extends Component {
+  render() {
+    return (
+      <div className="login">
+        <header className="login-header">
+          <img src={logo} alt="logo"></img>
+          <h1>React项目：后台管理系统</h1>
+          <Icon type="user" />
+        </header>
+        <section className="login-content">
+          <h2>用户登陆</h2>
+          <Form onSubmit={this.handleSubmit} className="login-form">
+        <Form.Item>
+            <Input
+              prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+              placeholder="Username"
+            />        
+        </Form.Item>
+        <Form.Item>
+            <Input
+              prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+              type="password"
+              placeholder="Password"
+            />
+        </Form.Item>
+        <Form.Item>
+          <Button type="primary" htmlType="submit" className="login-form-button">
+            Log in
+          </Button>
+        </Form.Item>
+      </Form>
+        </section>
+      </div>
+    )
+  }
+}
